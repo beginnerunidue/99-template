71521e244852ec0140cd86ace5b14713afda0e32
beginnerunidue <42777842+beginnerunidue@users.noreply.github.com>
2023-12-04 19:04:27 +0100

    Korrektur

--- src/components/RandomNumber.js
+++ src/components/RandomNumber.js
@@ -1,1 +1,19 @@
-function RandomNumber() {}
+import { useState } from "react";
+
+import generateRandomNum from "../utils/GenerateRandomNum";
+function RandomNumber() {
+  const [randomNum, setRandomNum] = useState(generateRandomNum());
+
+  const handleOnClick = () => {
+    setRandomNum(generateRandomNum());
+  };
+
+  return (
+    <div>
+      <h1>{randomNum}</h1>
+      <button onClick={handleOnClick}>Generate new random number</button>
+    </div>
+  );
+}
+
+export default RandomNumber;

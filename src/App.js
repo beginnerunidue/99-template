c901769ec3d53219c20a8868b7019ba85eaf29d5
beginnerunidue <42777842+beginnerunidue@users.noreply.github.com>
2023-12-05 17:35:27 +0100

    Completed

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
-import { useState } from "react";
-
 import "./App.css";
-
-import Button from "./components/Button";
-import Counter from "./components/Counter";
-
-const texts = [
-  "Click me 1",
-  "Click me 2",
-  "Click me 3",
-  "Click me 4",
-  "Click me 5",
-];
+import Persons from "./components/Persons";
 
 function App() {
-  console.log("App rendered");
-  const [clickCounter, setClickCounter] = useState(0);
-
-  const handleButtonClick = () => {
-    setClickCounter(clickCounter + 1);
-  };
-
   return (
     <div className="App">
-      <Counter clickNumber={clickCounter} />
-      {texts.map((text, index) => {
-        return (
-          <Button
-            incrementCounter={handleButtonClick}
-            text={text}
-            key={index}
-          />
-        );
-      })}
+      <Persons />
     </div>
   );
 }

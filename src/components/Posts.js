50ce73586c1ef6374eab16928ffc1d454f1935cf
beginnerunidue <42777842+beginnerunidue@users.noreply.github.com>
2023-12-07 15:08:08 +0100

    Section 22 Completed

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,20 +2,66 @@ import { useEffect, useState } from "react";
 
 import Post from "./Post";
 
+const API_URL = "https://jsonplaceholder.typicode.com/posts/";
+
 function Posts() {
   const [myPosts, setMyPosts] = useState(null);
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(true);
 
-  const API_URL = "https://jsonplaceholder.typicode.com/posts/";
+  //// 1. using async und await
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       const res = await fetch(API_URL);
+  //       const posts = await res.json();
+  //       setMyPosts(posts);
+  //     } catch (error) {
+  //       setError(error.message);
+  //     }
+  //     setIsLoading(false);
+  //   }
+  //   fetchData();
+  // }, []);
 
+  //// 2. using IIFE (Immediately Invoked Function Expressions)
+  // useEffect(() => {
+  //   (async function () {
+  //     try {
+  //       const res = await fetch(API_URL);
+  //       const posts = await res.json();
+  //       setMyPosts(posts);
+  //     } catch (error) {
+  //       setError(error.message);
+  //     }
+  //     setIsLoading(false);
+  //   })();
+  // }, []);
+
+  //// 3. using IIFE (Immediately Invoked Function Expressions)
+  //// with Arrow-Function
   useEffect(() => {
-    fetch(API_URL)
-      .then((response) => response.json())
-      .then((posts) => setMyPosts(posts))
-      .catch((error) => setError(error.message))
-      .finally(() => setIsLoading(false));
+    const fetchData = async () => {
+      try {
+        const res = await fetch(API_URL);
+        const posts = await res.json();
+        setMyPosts(posts);
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
   }, []);
+
+  //// 4. using fetch(API_URL).then.then
+  // useEffect(() => {
+  //   fetch(API_URL)
+  //     .then((response) => response.json())
+  //     .then((posts) => setMyPosts(posts))
+  //     .catch((error) => setError(error.message))
+  //     .finally(() => setIsLoading(false));
+  // }, []);
 
   // if (isLoading) {
   //   return <h1>Loading...</h1>;
